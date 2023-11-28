8919051e3d6109aba5fee0a52f8b1ba2bb2e5f4d
Christopher Baker <csb048@usask.ca>
2023-11-28 15:18:24 -0600

    minor refactoring, cleaned up code, added comments

--- src/cpuNumberCounting.cpp
+++ src/cpuNumberCounting.cpp
@@ -2,45 +2,45 @@
 #include <chrono>
 #include <cstdlib>
 
+int numberSearchCPU(std::vector<char> matrix, std::string target){
+    int numCount = 0;
+    for(auto character: matrix){
+        if(character == target[0]){
+            numCount++;
+        }
+    }return numCount;
+}
 
 int main(int argc, char* argv[]){
-    
+    // Initalizing user args
     if (argc != 4){
         std::cout << "Usage: " << argv[0] << " <input file>" << "<rows>" << "<number to find>" << std::endl;
         exit(1);
     }
-
     std::string fileName = argv[1];
     int rows = atoi(argv[2]);
-    std::string numberToFind = argv[3];
+    std::string targetChar = argv[3];
 
+    // creating timers
     std::chrono::milliseconds mallocDuration;
     std::chrono::milliseconds procDuration;
     
+    // pre-processing (loading array from file)
     auto fileStart = std::chrono::high_resolution_clock::now();
-
     auto matrix = testFileToArray(fileName, rows);
-
     auto fileEnd = std::chrono::high_resolution_clock::now();
-
     mallocDuration = std::chrono::duration_cast<std::chrono::milliseconds>(fileEnd - fileStart);
 
+    // running and timing CPU search of matrix
     auto procStart = std::chrono::high_resolution_clock::now();
-
-    int numCount = 0;
-
-    for(auto character: matrix){
-        if(character == numberToFind[0]){
-            numCount++;
-        }
-    }
-
+    auto numCount = numberSearchCPU(matrix, targetChar);
     auto procEnd = std::chrono::high_resolution_clock::now();
     procDuration = std::chrono::duration_cast<std::chrono::milliseconds>(procEnd-procStart);
 
+    // Outputing times
     std::cout << "CPU: Load from file time (THIS RUNS ON CPU): " << mallocDuration.count() << "ms" << std::endl;
     std::cout << "CPU: Proccessing Time: " << procDuration.count() << "ms" << std::endl;
-    std::cout << "CPU: Number of " << numberToFind << "'s in matrix: " << numCount << std::endl;
+    std::cout << "CPU: Number of " << targetChar << "'s in matrix: " << numCount << std::endl;
 
     return 0;
 }
