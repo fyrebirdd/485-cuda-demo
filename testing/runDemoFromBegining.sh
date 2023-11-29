711ad4b4509b1145d85cbfa9d1740ea3ff8f0510
Christopher Baker <csb048@usask.ca>
2023-11-28 22:19:32 -0600

    added helper script that runs all parts of the demo at once

---
+++ testing/runDemoFromBegining.sh
@@ -0,0 +1,11 @@
+if [ "$#" -ne 2 ]
+then
+  echo "USEAGE: runDemoFromBegining.sh <numberRows> <matrixFilename>"
+  exit 1
+fi
+
+echo "Generating $1 x $1 matrix..."
+python3 testFileGen.py $1 "matrix.txt"
+
+echo "Running Computaions..."
+python3 runTest.py "matrix.txt" $1 $2
